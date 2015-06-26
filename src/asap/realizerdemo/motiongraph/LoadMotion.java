e208f49e274dbab38ab338d2295a552a609a2d42
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2015-06-26 14:52:24 +0200

    LoadMotion zum testen angepasst

--- src/asap/realizerdemo/motiongraph/LoadMotion.java
+++ src/asap/realizerdemo/motiongraph/LoadMotion.java
@@ -2,13 +2,10 @@ package asap.realizerdemo.motiongraph;
 
 import static asap.realizerdemo.motiongraph.Util.X;
 import static asap.realizerdemo.motiongraph.Util.Y;
-import static asap.realizerdemo.motiongraph.Util.Z;
 import hmi.animation.ConfigList;
 import hmi.animation.SkeletonInterpolator;
-import hmi.math.Mat3f;
 import hmi.math.Quat4f;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -19,14 +16,16 @@ import java.util.Set;
  */
 public class LoadMotion {
 
+    public static int test = 0;
     
+    public static final float Y_DEFAULT = 0.975211761f;
 
     public static List<SkeletonInterpolator> loadMotion(String[] files) throws IOException {
 
-        List<SkeletonInterpolator> motions = new LinkedList<SkeletonInterpolator>();
+        List<SkeletonInterpolator> motions = new LinkedList<>();
 
         for (String file : files) {
-
+            
             SkeletonInterpolator skeletonInterpolator = SkeletonInterpolator.read("idle", file);
 
             LoadMotion.fixRootTransformation(skeletonInterpolator);
@@ -34,44 +33,44 @@ public class LoadMotion {
 
             LoadMotion.fixJoints(skeletonInterpolator);
             System.out.println(file + " Joints fixed");
-            
+
             motions.add(skeletonInterpolator);
             System.out.println(file + " loaded");
 
+            test++;
         }
-        
+
         System.out.println("motions loaded");
-        
+
         return motions;
     }
-
+    
     public static void fixRootTransformation(SkeletonInterpolator motion) {
         ConfigList newConfig = new ConfigList(motion.getConfigSize());
+            
 
         //float zRot = 0;
-
         for (int i = 0; i < motion.getConfigList().size(); i++) {
             float[] config = motion.getConfig(i);
             double time = motion.getTime(i);
 
-            config[Y] = 0.975211761f;
-
-            float[] quat = {config[3],config[4],config[5],config[6]};
+            config[Y] = Y_DEFAULT;
+            config[X] = config[X]+test;
             
+            float[] quat = {config[3], config[4], config[5], config[6]};
+
             float[] rollPitchYaw = new float[3];
             Quat4f.getRollPitchYaw(quat, rollPitchYaw);
             //zRot = rollPitchYaw[1]=0;
             Quat4f.setFromRollPitchYaw(quat, rollPitchYaw[0], rollPitchYaw[1], rollPitchYaw[2]);
-            config[3]=quat[0];
-            config[4]=quat[1];
-            config[5]=quat[2];
-            config[6]=quat[3];
-            
+            config[3] = quat[0];
+            config[4] = quat[1];
+            config[5] = quat[2];
+            config[6] = quat[3];
+
             newConfig.addConfig(time, config);
         }
 
-        
-        
         motion.setConfigList(newConfig);
     }
 
@@ -87,5 +86,8 @@ public class LoadMotion {
         joints.remove("r_acromioclavicular");
 
         motion.filterJoints(joints);
+    }
+
+    private LoadMotion() {
     }
 }
